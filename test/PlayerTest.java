8ad28b39ac9309980b0791c57bceae8282fe18b2
Gallien <gwong@et.esiea.fr>
2017-03-02 21:19:01 +0100

    Amélioration PlayerTest

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -1,20 +1,27 @@
 import static org.junit.Assert.*;
 
+import java.util.Objects;
+
 import org.junit.Test;
 
 public class PlayerTest {
+	
+Player name = new Player(1,"Test");
 
+	
 	@Test
 	public void testPlayer() {
-		
-		Player name1 = new Player(1,"lol");
-		Player name2 = new Player(1,"lol");
+		Player name2 = new Player(1,"Test");
 
+		assertTrue(this.name.equals(name2));
+		assertEquals(this.name.hashCode(),name2.hashCode());
 	}
 
 	@Test
 	public void testGetName() {
-		//fail("Not yet implemented");
+		Player name2 = new Player(1,this.name.name);
+		assertEquals(name2.getName(),this.name.getName());
 	}
+
 
 }
