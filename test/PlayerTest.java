36fd0bbe7c0d55d27d95d37c62d73c11839314a0
Gallien <gwong@et.esiea.fr>
2017-03-02 17:33:10 +0100

    Correctif : Maven ajouté + PlayerTest ajouté

---
+++ test/PlayerTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() {
+		
+		Player name1 = new Player(1,"lol");
+		Player name2 = new Player(1,"lol");
+
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+}
