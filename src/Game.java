f81c187c8806e9c34dcc239fcf21305c7cc2c8a8
Gallien <gwong@et.esiea.fr>
2017-03-03 02:58:26 +0100

    Merge and push (with test)

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,4 @@
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
